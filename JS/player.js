eb8a02d9c734052809f6222afdaa1fb436725fce
Zsolt HORVATH <h.zsola@gmail.com>
2021-07-10 14:51:27 +0200

    Iteration N°1 - Creating Player object

---
+++ JS/player.js
@@ -0,0 +1,40 @@
+// This class is representing the little figurine on the board.
+//This is the player.
+//This object knows nothing about the objects found in the main JS file.
+//I seperated it entirely from it
+
+class Player{
+    constructor(name){
+        this.name = name;
+    }
+
+    x=0;
+    y=0
+    
+    //Moving towards X direction
+    moveForward(){
+        this.x+=1;
+    }
+
+    //Moving towards negative Y direction
+    moveLeft(){
+        this.y-=1;
+    }
+
+    //Moving towards positive Y direction
+    moveRight(){
+        this.y+=1;
+    }
+
+    //Moving towards negativ X direction
+    moveBack(){
+        this.x-=1;
+    }
+
+    //Set the starting position at the beginning of each round
+    setStartingPosition(posY, posX){
+        this.y=posY;
+        this.x=posX;
+    }
+
+}
